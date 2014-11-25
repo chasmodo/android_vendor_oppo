13466d01cc98ba26436317f410e50a2b8cddbd6b
Ethan Chen <intervigil@gmail.com>
2014-11-24 23:09:29 -0800

    apq8064-common: Update Adreno libraries
    * Package version: razor-lrx21p

--- apq8064-common/apq8064-common-vendor-blobs.mk
+++ apq8064-common/apq8064-common-vendor-blobs.mk
@@ -51,12 +51,13 @@ PRODUCT_COPY_FILES += \
     vendor/oppo/apq8064-common/proprietary/vendor/lib/egl/libplayback_adreno.so:system/vendor/lib/egl/libplayback_adreno.so \
     vendor/oppo/apq8064-common/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
     vendor/oppo/apq8064-common/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+    vendor/oppo/apq8064-common/proprietary/vendor/lib/libbccQTI.so:system/vendor/lib/libbccQTI.so \
+    vendor/oppo/apq8064-common/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
     vendor/oppo/apq8064-common/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
     vendor/oppo/apq8064-common/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
-    vendor/oppo/apq8064-common/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
     vendor/oppo/apq8064-common/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
     vendor/oppo/apq8064-common/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
-    vendor/oppo/apq8064-common/proprietary/vendor/lib/libllvm-a3xx.so:system/vendor/lib/libllvm-a3xx.so \
+    vendor/oppo/apq8064-common/proprietary/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
     vendor/oppo/apq8064-common/proprietary/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
     vendor/oppo/apq8064-common/proprietary/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
     vendor/oppo/apq8064-common/proprietary/vendor/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
